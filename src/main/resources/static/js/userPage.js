ef672c3e1ecfff6e3a30faf472c3ad2c91e071ae
Denis Pronin <de.pronin@gmail.com>
2022-05-19 07:32:15 +0300

    userPage.js refactoring

--- src/main/resources/static/js/userPage.js
+++ src/main/resources/static/js/userPage.js
@@ -1,31 +1,36 @@
 //This script populates the header and single table row with user's data
-fetch('http://localhost:8080/user/json')
-    .then(response => {
-        response.json().then(user => {
+const populateHeader = () => {
+    fetch('http://localhost:8080/user/json')
+        .then(response => {
+            response.json().then(user => {
             let roles = "";
-            if (user.roles.length > 0) {
-                for (const role of user.roles) {
-                    roles += role.role;
-                    roles += " ";
-                }
-            } else {
-                roles += "No role";
+            for (const role of user.roles) {
+                roles += role.role;
+                roles += " ";
             }
 
-            // Populating header
             document.querySelector("#headerName").text = user.name;
             document.querySelector("#headerRole").text = roles;
-
-            // Populating table
-            document.querySelector("#tableId").innerHTML = user.id;
-            document.querySelector("#tableName").innerHTML = user.name;
-            document.querySelector("#tablePassword").innerHTML = user.password;
-            document.querySelector("#tableAge").innerHTML = user.age;
-            document.querySelector("#tableRoles").innerHTML = roles;
-
-            // Displaying Admin Panel link
-            if (roles.includes("ROLE_ADMIN")) {
-                document.querySelector("#admin").style.display = "block";
-            }
+            })
         })
-    })
+}
+
+const populateSingleRowTable = () => {
+    fetch('http://localhost:8080/user/json')
+        .then(response => {
+            response.json().then(user => {
+                document.querySelector("#tableId").innerHTML = user.id;
+                document.querySelector("#tableName").innerHTML = user.name;
+                document.querySelector("#tablePassword").innerHTML = user.password;
+                document.querySelector("#tableAge").innerHTML = user.age;
+                document.querySelector("#tableRoles").innerHTML = document.querySelector("#headerRole").innerHTML;
+                // Displaying Admin Panel link
+                if (document.querySelector("#headerRole").text.includes("ROLE_ADMIN")) {
+                    document.querySelector("#admin").style.display = "block";
+                }
+            })
+       })
+}
+
+populateHeader()
+populateSingleRowTable()
